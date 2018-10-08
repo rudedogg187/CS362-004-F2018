8db86eaac61efc86c09c805de5c4deb7a4c09b58
geographicdesign <joshua.l.hughes.pro@gmail.com>
2018-10-07 20:33:48 -0700

    refactored six card effects into functions

--- projects/hughejos/dominion/dominion_helpers.h
+++ projects/hughejos/dominion/dominion_helpers.h
@@ -9,6 +9,18 @@ int discardCard(int handPos, int currentPlayer, struct gameState *state,
 		int trashFlag);
 int gainCard(int supplyPos, struct gameState *state, int toFlag, int player);
 int getCost(int cardNumber);
+
+
+
+int adventureEffect(struct gameState *state);
+int councilRoomEffect(struct gameState *state, int handPos);
+int featEffect(struct gameState *state, int handPos, int choice);
+int mineEffect(struct gameState *state, int handPos, int choice1, 
+		int choice2);
+int remodelEffect(struct gameState *state, int handPos, int choice1, 
+		int choice2);
+int smithyEffect(struct gameState *state, int handPos);
+
 int cardEffect(int card, int choice1, int choice2, int choice3, 
 	       struct gameState *state, int handPos, int *bonus);
 
