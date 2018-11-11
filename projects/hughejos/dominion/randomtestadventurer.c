a40ad7cad6ae8fdc4c1e67e597d54ec623887606
geographicdesign <joshua.l.hughes.pro@gmail.com>
2018-11-11 15:03:20 -0800

    assignment 4 initial push

---
+++ projects/hughejos/dominion/randomtestadventurer.c
@@ -0,0 +1,16 @@
+#include "dominion.h"
+#include "dominion_helpers.h"
+#include "rngs.h"
+
+#include <assert.h>
+#include <stdio.h>
+#include <string.h>
+
+
+int main() {
+	printf("This is random card tester: Adventurer\n");
+
+
+	return 0;
+}
+
